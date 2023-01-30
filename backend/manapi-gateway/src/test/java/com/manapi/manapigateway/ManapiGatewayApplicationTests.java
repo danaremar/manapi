e8454424d03d0a2c25485841879a2b4e84c0b931
danaremar <56022332+danaremar@users.noreply.github.com>
2023-01-30 11:55:22 +0100

    Tests failed

--- backend/manapi-gateway/src/test/java/com/manapi/manapigateway/ManapiGatewayApplicationTests.java
+++ backend/manapi-gateway/src/test/java/com/manapi/manapigateway/ManapiGatewayApplicationTests.java
@@ -1,10 +1,13 @@
 package com.manapi.manapigateway;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
-class ManapiGatewayApplicationTests {
+// @RunWith(SpringJUnit4ClassRunner.class)
+public class ManapiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
