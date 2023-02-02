010143067baf6c74b9fba600ff1582a537f4601b
danaremar <56022332+danaremar@users.noreply.github.com>
2023-02-02 14:03:10 +0100

    Added project microservice
    - Added project microservice
    - Changed banner when it runs
    - Changed spring metadata to parent (applied to all projects)

---
+++ backend/manapi-project/src/test/java/com/manapi/manapiproject/ManapiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manapi.manapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManapiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
